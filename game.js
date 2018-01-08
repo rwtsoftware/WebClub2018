99abb22e2ff0da353dd41bb7db0664c9eb51dcdd
Ryan Taylor <ryan@rwtsoftware.com>
2018-01-08 00:19:04 +0000

    Initial Game

---
+++ game.js
@@ -0,0 +1,18 @@
+var clickHereButtonHandler = function() {
+	var current_value = Number.parseInt($('#click-here-count').text());
+	$('#click-here-count').text(current_value + 1);
+};
+
+var secondCounterHandler = function() {
+    var current_value = Number.parseInt($('#second-timer-count').text());
+    $('#second-timer-count').text(current_value + 1);
+};
+
+var setEventHandlers = function() {
+	$('#click-here-button').click(clickHereButtonHandler);
+	setInterval(secondCounterHandler, 1000);
+};
+
+$( document ).ready(function() {
+    setEventHandlers();
+});
