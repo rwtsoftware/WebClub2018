ea75879ba8435e4ffc8d6015a0b7db09158826fc
www-data <www-data@developer-jamar.c.web-club-190922.internal>
2018-01-24 20:19:31 +0000

    jamar submitted these changes at 2018-01-24 15:19:31

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 var clickHereButtonHandler = function() {
 	var current_value = Number.parseInt($('#click-here-count').text());
-	$('#click-here-count').text(current_value + 1);
+	$('#click-here-count').text(current_value + 10);
 };
 
 var secondCounterHandler = function() {
     var current_value = Number.parseInt($('#second-timer-count').text());
-    $('#second-timer-count').text(current_value + 1);
+    $('#second-timer-count').text(current_value + 10);
 };
 
 var setEventHandlers = function() {
