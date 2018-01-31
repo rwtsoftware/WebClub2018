a8bf7bf7457600b604172ebce7e97163a713deda
www-data <www-data@developer-rodgeris.c.web-club-190922.internal>
2018-01-31 20:40:26 +0000

    rodgeris saved these changes at 2018-01-31 15:40:26

--- game.js
+++ game.js
@@ -1,16 +1,16 @@
 var clickHereButtonHandler = function() {
 	var current_value = Number.parseInt($('#click-here-count').text());
-	$('#click-here-count').text(current_value + 1);
+	$('#click-here-count').text(current_value +   10000000000000000);
 };
 
 var secondCounterHandler = function() {
     var current_value = Number.parseInt($('#second-timer-count').text());
-    $('#second-timer-count').text(current_value + 1);
+    $('one secondtimer').text(current_value + 10000000000000000000000000);
 };
 
 var setEventHandlers = function() {
 	$('#click-here-button').click(clickHereButtonHandler);
-	setInterval(secondCounterHandler, 1000);
+	setInterval(secondCounterHandler, 1000000000000000000);
 };
 
 $( document ).ready(function() {
