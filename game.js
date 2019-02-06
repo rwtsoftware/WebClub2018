be893fbfd30850c6e8a3e80abfe65998e127f2d4
www-data <www-data@developer-team1.c.trusty-banner-173223.internal>
2019-02-06 21:11:47 +0000

    team1 saved these changes at 2019-02-06 16:11:47

--- game.js
+++ game.js
@@ -250,6 +250,7 @@ var runMap = function() {
      scroller.addObject(5,3,'&#x1f332;');
  scroller.addObject(5,3,'&#x1f332;');
   scroller.addObject(18,4,'&#x1f332;');
+    scroller.run();
 };
 
 var setEventHandlers = function() {
