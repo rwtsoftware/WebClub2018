6345c5154cffad30be8a3e6a356152598c772b25
www-data <www-data@developer-team2.c.trusty-banner-173223.internal>
2019-03-13 19:56:40 +0000

    team2 saved these changes at 2019-03-13 15:56:40

--- game.js
+++ game.js
@@ -270,6 +270,8 @@ var RunOverBerries = function() {
 var RunOverHole = function() {
 	var current_value = Number.parseInt($('#berries-collected-count').text());
   $('#berries-collected-count').text(current_value - 10 );
+  	var current_value = Number.parseInt($('#cars-collected-count').text());
+  $('#cars-collected-count').text(current_value - 10 );
   
 };	
 var runMap = function() {
