65fa5e7b1a11c3f3bd8058690051ad694a19770e
www-data <www-data@developer-team2.c.trusty-banner-173223.internal>
2019-03-20 20:09:04 +0000

    team2 saved these changes at 2019-03-20 16:09:03

--- scroller_helper.js
+++ scroller_helper.js
@@ -26,7 +26,7 @@ var collisionDetect = function(myMap) {
     let o = myMap.objects[k];
     if (myMap.player.x === o.x && myMap.player.y === o.y) {
     	console.log(o);
-    	 myMap.objects[k]={};
+    	 myMap.objects[k].x=-10;
     	o.callback();
     }
   }
